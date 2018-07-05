856ca5222d17f42ec06eaedc828091c988e19313
SajjadMohammed <30750038+SajjadMohammed@users.noreply.github.com>
2018-07-05 11:49:54 +0300

    Add project files.

---
+++ LoadingImage/DownloadAsync.cs
@@ -0,0 +1,34 @@
+﻿using Android.Graphics;
+using Android.Views;
+using Android.Views.Animations;
+using Android.Widget;
+
+using System.Net.Http;
+
+namespace LoadingImage
+{
+    internal class DownloadAsync
+    {
+        ProgressBar mypProgressBar;
+        ImageView myImage;
+        readonly MainActivity mainActivity;
+
+        public DownloadAsync(MainActivity mainActivity, ImageView myImage, ProgressBar mypProgressBar)
+        {
+            this.mainActivity = mainActivity;
+            this.mypProgressBar = mypProgressBar;
+            this.myImage = myImage;
+        }
+
+        public async void DownloadImageAsync(string url)
+        {
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetByteArrayAsync(url);
+            var bmp = await BitmapFactory.DecodeByteArrayAsync(content, 0, content.Length);
+            var anim = AnimationUtils.LoadAnimation(mainActivity, Android.Resource.Animation.FadeIn);
+            myImage.Animation = anim;
+            myImage.SetImageBitmap(bmp);
+            mypProgressBar.Visibility = ViewStates.Gone;
+        }
+    }
+}
---
+++ LoadingImage/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+using V7ToolBar = Android.Support.V7.Widget.Toolbar;
+
+namespace LoadingImage
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        V7ToolBar mainToolBar;
+        ProgressBar myProgressBar;
+        ImageView myImage;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            mainToolBar = FindViewById<V7ToolBar>(Resource.Id.toolbar);
+            SetSupportActionBar(mainToolBar);
+            //
+            myProgressBar = FindViewById<ProgressBar>(Resource.Id.myProgressBar);
+            myImage = FindViewById<ImageView>(Resource.Id.myImage);
+
+            //
+            var url = "https://avatars2.githubusercontent.com/u/30750038?s=460&v=4";
+            DownloadAsync downloadAsync = new DownloadAsync(this, myImage, myProgressBar);
+            downloadAsync.DownloadImageAsync(url);
+        }
+    }
+}
